8876f65ced83add8c0a5e0903e7607b099462dc2
Peter-Chong <wlchong@edu.uwaterloo.ca>
2022-05-15 22:15:00 -0700

    Move folders around

---
+++ algorithms/Experiment/Linear_Regression.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_regression
+
+#from Utils.data_manipulation import train_test_split
+from algorithms.Supervised import regression
+
+def main():
+    
+    X, y = make_regression(n_features = 1, noise = 20)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+    n_samples, n_features = np.shape(X)
+
+    model = LinearRegression(epochs=100)
+    model.fit(X_train, y_train)
+
+    print('ok')
+
+
+if __name__ == "__main__":
+    main()
---
+++ algorithms/Utils/data_manipulation.py
@@ -0,0 +1,19 @@
+import math
+from posixpath import split
+import numpy as np
+
+def shuffle_data(X, y, seed=None):
+    if seed:
+        np.random.seed(seed)
+    idx = np.arrange(X.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
+
+def train_test_split(X, y, test_size, shuffle=True, seed=None):
+    if shuffle:
+        X, y = shuffle_data(X, y, seed)
+    split_i = len(y) - int(len(y) * test_size)
+    X_train, X_test = X[:split_i], X[split_i:]
+    y_train, y_test = y[:split_i], y[split_i:]
+
+    return X_train, X_test, y_train, y_test
